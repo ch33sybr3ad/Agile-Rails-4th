3bfd6730962065f840e9e00f19a82914e34cae27
Jason <jzhao.690@gmail.com>
2015-05-22 08:54:19 -0700

    scaffold products, image_tags

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+require 'faker'
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+10.times do
+Product.create(
+  title: Faker::Commerce.product_name,
+  description: Faker::Lorem.sentence,
+  image_url: Faker::Company.logo,
+  price: Faker::Commerce.price,
+  )
+end
