73ccd8bdb997193ce06410284ded7f36033ef391
Jason <jzhao.690@gmail.com>
2015-05-23 12:49:22 -0700

    add tests

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+
+  validates_presence_of :title, :description, :image_url
+  validates :price, :numericality => { greater_than_or_equal_to: 0.01 } 
+  validates_uniqueness_of :title
+  validates :image_url, :format => {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPQ or PNG image',
+  }
+
 end
