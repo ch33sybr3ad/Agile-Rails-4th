1d686737bab355d2aa56acd8252df48e43750897
Jason <jzhao.690@gmail.com>
2015-05-23 13:54:54 -0700

    add time, styled layout

--- depot/test/controllers/store_controller_test.rb
+++ depot/test/controllers/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 4 
+    assert_select 'h1', 'Your Pragmatic Catalog' 
+    assert_select 'td', /\$[,\d]+\.\d\d/
   end
+
+
 
 end
